c6a72a696c7a53c53212bfcbf5f407f90d79169f
Katze <73298240+salzigekatze@users.noreply.github.com>
2021-03-20 06:10:30 +0300

    Update number_guess.py
    Added extra help by telling if its higher or a lower value

--- number_guess.py
+++ number_guess.py
@@ -4,49 +4,51 @@ import random
 Simple number guess game with Python and Random Module
 There is only one tip for help and it is shown to the player 
 after the first wrong guess. It shows if the number is even or odd.
+It also tells if the guess it lower or higher then the number
 '''
 
-print(' ')
-print('A simple number guessing game')
-print(' ')
-print('Lets see if you are lucky')
-print(' ')
+
+print('A simple number guessing game\n')
+print('Lets see if you are lucky\n')
+
 
 z = int(input('Enter number range: '))
 x = random.randrange(z)
 
-print(' ')
+print('\n')
 
 y = int(input('Enter a number: '))
 
-print(' ')
+print('\n')
 
 firsttry = True
 
 
 while (x!=y):
 
-    print('False guess')
+    print('False guess\n')
+
+    if y>x:
+        print("Make a lower guess\n")
+        pass
+    if y<x:
+        print("Make a higher guess\n")
+        pass
     
     if firsttry:
-        print(' ')
-        print('Here is a quick help')
-        print(' ')
+        print('Here is a quick help\n')
 
         if (x%2==0):
-            print('It is a even number')
-            print(' ')
+            print('It is a even number\n')
             firsttry = False
             
         else:
-            print('It is an odd number')
-            print(' ')
+            print('It is an odd number\n')
             firsttry = False
 
     y = int(input('Enter a new number: '))
-    print(' ')
+    print('\n')
 
 
 if (x == y):
     print('Correct guess! YOU WIN!')
-
